33a17dabbb1b9b1def6ec1612e28ac15bef12713
dorian.hodin <Dorian/2020>
2023-10-06 22:06:58 +0200

    Change routes for session call

--- fantom_games/lib/connection/try_connection_with_id_in_api.dart
+++ fantom_games/lib/connection/try_connection_with_id_in_api.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 Future<String> tryConnectionWithSession(int id) async {
   try {
     Uri uri = Uri.https('codefirst.iut.uca.fr',
-        '/containers/fantom_games-deploy_api/userConnection/$id');
+        '/containers/fantom_games-deploy_api/userForId/$id');
     http.Response response = await http.get(uri,
       headers: <String, String>{
         'Access-Control-Allow-Origin': '*',
