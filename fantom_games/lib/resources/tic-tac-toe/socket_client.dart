4c4d41b62cea8bb7e8d152202b3aee7c3dfd7174
dohodin <WrapPoulet2022>
2024-03-03 12:22:23 +0100

    Putting the correct link for socket Client

--- fantom_games/lib/resources/tic-tac-toe/socket_client.dart
+++ fantom_games/lib/resources/tic-tac-toe/socket_client.dart
@@ -3,7 +3,6 @@ import 'package:socket_io_client/socket_io_client.dart' as io;
 class SocketClient {
   io.Socket? socket;
   static SocketClient? _instance;
-/*
   SocketClient._internal() {
     socket = io.io('https://sockettictactoe.fantomgames.eu', <String, dynamic>{
       'transports': ['websocket'],
@@ -11,9 +10,8 @@ class SocketClient {
     });
     socket!.connect();
   }
-*/
 
-
+  /*
   SocketClient._internal() {
     socket = io.io('http://localhost:3000', <String, dynamic>{
       'transports': ['websocket'],
@@ -21,6 +19,8 @@ class SocketClient {
     });
     socket!.connect();
   }
+
+   */
 
 
 
